9c920c63b9eac3b492a4512b36cab4e189d79304
maxis <maxis1718@gmail.com>
2014-09-19 09:06:23 -0700

    Add submodules and installer

---
+++ submodules.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git submodule add git@github.com:robbyrussell/oh-my-zsh.git submodules/oh-my-zsh
+git submodule add git@github.com:seebi/dircolors-solarized.git submodules/dircolors-solarized
+git submodule add git@github.com:altercation/solarized.git submodules/solarized
+git submodule add git@github.com:seebi/tmux-colors-solarized.git submodules/tmux-colors-solarized
+git submodule add git@github.com:Lokaltog/powerline.git submodules/powerline

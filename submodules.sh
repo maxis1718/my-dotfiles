410a73d35b8b8170328c8357dbdd1581788842c3
Ting-hui Kao <maxiskao@punchingmunching.corp.sg3.yahoo.com>
2014-12-17 06:50:24 +0000

    Add submodule auto-install

--- submodules.sh
+++ submodules.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-git submodule add git@github.com:robbyrussell/oh-my-zsh.git submodules/oh-my-zsh
-git submodule add git@github.com:seebi/dircolors-solarized.git submodules/dircolors-solarized
-git submodule add git@github.com:altercation/solarized.git submodules/solarized
-git submodule add git@github.com:seebi/tmux-colors-solarized.git submodules/tmux-colors-solarized
-git submodule add git@github.com:Lokaltog/powerline.git submodules/powerline
+git submodule update --init submodules/solarized
+git submodule update --init submodules/tmux-colors-solarized
+git submodule update --init submodules/tmux-powerline
+git submodule update --init submodules/powerline
+git submodule update --init submodules/dircolors-solarized
+git submodule update --init submodules/oh-my-zsh
+
+#git submodule add git@github.com:robbyrussell/oh-my-zsh.git submodules/oh-my-zsh
+#git submodule add git@github.com:seebi/dircolors-solarized.git submodules/dircolors-solarized
+#git submodule add git@github.com:altercation/solarized.git submodules/solarized
+#git submodule add git@github.com:seebi/tmux-colors-solarized.git submodules/tmux-colors-solarized
+#git submodule add git@github.com:Lokaltog/powerline.git submodules/powerline
